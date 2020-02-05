113324d49dda389e2feae2cf1850036db7f33969
Gil Beyruth <firstpixel@gmail.com>
2020-02-05 12:02:17 -0300

    Adding user CRUD

--- src/main/java/com/firstpixel/backend/controller/UserController.java
+++ src/main/java/com/firstpixel/backend/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.firstpixel.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +13,10 @@ import com.firstpixel.backend.entity.User;
 import com.firstpixel.backend.repository.UserRepository;
 
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -19,8 +26,42 @@ public class UserController {
 	UserRepository userRepository;
 
 	@RequestMapping(value = "", method = RequestMethod.GET)
-	public List<User> list(){
-		return userRepository.findAll();
+	public Page<User> list(
+			@RequestParam("page") int page,
+			@RequestParam("size") int size
+			){
+		PageRequest pageable = PageRequest.of(page, size);
+		return  userRepository.findAll(pageable);
+		
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public User save( @RequestBody User user ){
+		
+		return userRepository.save(user);
+		
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.PUT)
+	public User edit( @RequestBody User user ){
+		
+		return userRepository.save(user);
+		
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void delete( @PathVariable Long id ){
+		
+		 userRepository.deleteById(id);
+		
+	}
+	
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Optional<User> detail( @PathVariable Long id ){
+		
+		 return userRepository.findById(id);
+		
 	}
 	
 }
